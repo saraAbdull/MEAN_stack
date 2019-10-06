df1454763788855a6ad46aef204d2ddd4860ed3f
saraAbdull <alkharashi.sara@gmail.com>
2019-10-06 03:41:11 +0300

    adding cars_cats in express

---
+++ express/cars_cats/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get('/', (request, response) => {
+    response.redirect("/index.html")
+});
+app.listen(8000, () => console.log("listening on port 8000"));
+app.use(express.static(__dirname + "/static"));
+console.log(__dirname)
