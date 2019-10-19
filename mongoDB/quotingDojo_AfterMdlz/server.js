100f0f299cbdbe703555d209199bd75bfb572c2a
saraAbdull <alkharashi.sara@gmail.com>
2019-10-19 03:44:57 +0300

    Adding quoting dojo after modularize client

--- mongoDB/quotingDojo_AfterMdlz/server.js
+++ mongoDB/quotingDojo_AfterMdlz/server.js
@@ -4,8 +4,8 @@ const app = express();
 require("./server/models/quote.js")
    
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/static'));
+app.set('views', __dirname + '/client/views');
+app.use(express.static(__dirname + '/client/static'));
 app.use(express.urlencoded({extended: true}));
 
 
